500f70c11f4919593e4e42b6d6b1919e9f40c68d
Shujaa Gideon <shujaagideon@gmail.com>
2023-09-30 12:24:36 +0300

    simple adjustments🪶

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -16,14 +16,14 @@ const Contact = () => {
             </Canvas>
         </React.Suspense>
       </div>
-      <div className='h-screen fixed flex justify-center items-center w-full px-20 z-30 bg-transparent'>
+      <div className='h-screen fixed flex justify-center items-center w-full px-5 lg:px-20 z-30 bg-transparent'>
           <div className="w-full my-auto">
-            <h2 className='uppercase text-6xl mb-24 font-bold text-slate-100'>Get In touch with us</h2>
+            <h2 className='uppercase text-4xl md:text-6xl mb-24 font-bold text-slate-100'>Get In touch with us</h2>
 
-            <form action="" className='w-3/4'>
-                <div className="flex w-full h-fit mb-10 justify-between">
-                    <input placeholder='name' className='p-3 px-6 h-20 w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
-                    <input placeholder='name' className='p-3 px-6 h-20 w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
+            <form action="" className='w-full md:w-3/4'>
+                <div className="flex flex-col md:flex-row w-full h-fit mb-10 justify-between">
+                    <input placeholder='name' className='p-3 px-6 h-20 w-full mb-4 md:w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
+                    <input placeholder='name' className='p-3 px-6 h-20 w-full mb-4 md:w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
                 </div>
                 <textarea placeholder="Write a message ..." className="p-6 w-full rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100" name="" id="" cols="30" rows="10"></textarea>
             </form>
