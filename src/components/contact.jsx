a916a3a9e41205a64da76db9361d0aa25e7bd9df
Shujaa Gideon <shujaagideon@gmail.com>
2023-09-30 12:45:24 +0300

    simple adjustments🪶

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -20,12 +20,12 @@ const Contact = () => {
           <div className="w-full my-auto">
             <h2 className='uppercase text-4xl md:text-6xl mb-14 lg:mb-24 font-bold text-slate-100'>Get In touch with us</h2>
 
-            <form action="" className='w-full md:w-4/5 lg:w-3/4'>
+            <form action="" className='w-full md:w-4/5 lg:w-3/4 h-fit'>
                 <div className="flex flex-col md:flex-row w-full h-fit mb-10 justify-between">
-                    <input placeholder='name' className='p-3 px-6 h-20 w-full mb-4 md:w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
-                    <input placeholder='name' className='p-3 px-6 h-20 w-full mb-4 md:w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
+                    <input placeholder='name' className='p-3 px-6 h-14 md:h-20 w-full mb-4 md:w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
+                    <input placeholder='name' className='p-3 px-6 h-14 md:h-20 w-full mb-4 md:w-[47%] rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100'/>
                 </div>
-                <textarea placeholder="Write a message ..." className="p-6 w-full rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100" name="" id="" cols="30" rows="10"></textarea>
+                <textarea placeholder="Write a message ..." className=" h-40 md:h-fit p-6 w-full rounded-3xl backdrop:blur-md text-slate-100 bg-transparent border border-slate-100  placeholder:text-slate-100" name="" id="" cols="30" rows="10"></textarea>
             </form>
           </div>
       </div>
