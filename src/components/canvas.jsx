0e51161508cf30263de2ed6cb453feceb2266fc9
Shujaa Gideon <shujaagideon@gmail.com>
2023-11-02 20:44:27 +0300

    simple adjustments🪶

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -7,7 +7,7 @@ import { Circle, Loader, Scroll, ScrollControls, Sparkles, useScroll } from "@re
 import { getProject, val, types as t } from "@theatre/core";
 import bgImg from '../assets/Background.jpg'
 import * as THREE from 'three'
-import sceneState from '../assets/state13.json'
+import sceneState from '../assets/state14.json'
 import mobileState from '../assets/mobileState.json'
 
 import {
