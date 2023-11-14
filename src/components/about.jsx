4a70061479cf95289b7fddea581d6e5331e2a97d
Shujaa Gideon <shujaagideon@gmail.com>
2023-11-14 08:00:20 +0300

    simple adjustments🪶

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -26,25 +26,25 @@ const About = () => {
                 </Canvas>
             </React.Suspense>
         </div>
-        <div className="h-screen fixed z-20 w-full p-2 md:p-5 lg:p-20 bg-transparent flex overflow-hidden">
+        <div className="h-screen fixed z-20 w-full p-2 md:p-5 lg:p-20 bg-transparent flex overflow-hidden ">
             <img src={ximage} alt="" className="absolute top-0 right-[10%] h-[50%]" />
-            <div className="w-full overscroll-y-scroll md:w-4/5  xl:w-[45%] h-fit text-slate-100 backdrop-blur-md ml-auto lg:mt-24 xl:mt-28 text-sm md:text-base md:mt-auto border border-slate-100 rounded-xl p-4 md:p-6">
-                <p className="mb-4 text-center">TACTECH New Media Solutions is a burgeoning assembly of
+            <div className="w-full mt-20 overscroll-y-scroll md:w-4/5  xl:w-[45%] 2xl:min-h-[50%] 2xl:mt-64 h-fit text-slate-100 backdrop-blur-md ml-auto lg:mt-24 xl:mt-28 text-sm md:text-base md:mt-auto border border-slate-100 rounded-xl p-4 md:p-6">
+                <p className="mb-4 text-center lg:text-left">TACTECH New Media Solutions is a burgeoning assembly of
                     acclaimed artists, forward-thinking visionaries, inventive coders,
                     skillful animators, resourceful designers, and adept engineers.
                 </p>
-                <p className="mb-4 text-center">
+                <p className="mb-4 text-center lg:text-left">
                     We are dedicated to creating and delivering immersive, interactive,
                     and captivating experiences through the utilization of
                     groundbreaking and transformative technologies.
                 </p>
-                <p className="mb-4 text-center">
+                <p className="mb-4 text-center lg:text-left">
                     Our expertise spans across TTL Campaigns (both Online & Offine),
                     XR, MR, AR, VR, Interactive installations, Immersive launches &
                     Shows, along with temporary and permanent installations
                     adaptable to a multitude of industries.
                 </p>
-                <p className="mb-4 text-center">
+                <p className="mb-4 text-center lg:text-left">
                     As a collective, we are driven by our passion to transcend the
                     conventional limits of new media solutions, crafting unforgettable
                     experiences that make a lasting imprint
