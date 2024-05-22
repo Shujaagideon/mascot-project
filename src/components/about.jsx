858c7358296bb3887ab068438ba2445160a9f76c
Shujaa Gideon <shujaagideon@gmail.com>
2024-05-22 09:27:47 +0300

    complete-for-real

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,8 +7,6 @@ import text from '../assets/approach.webp';
 import { Sparkles } from '@react-three/drei';
 import React from 'react';
 import Aboutmascot from './aboutmascot';
-import { EffectComposer, Noise } from '@react-three/postprocessing';
-import { BlendFunction } from 'postprocessing';
 import ximage from '../assets/x1.png';
 // import bgImage from '../assets/approach.webp';
 
@@ -36,27 +34,27 @@ const About = () => {
           className='absolute top-0 right-[10%] h-[35%] scale-50'
         />
         <div className='w-full mt-20 overscroll-y-scroll md:w-4/5  xl:w-[45%] 2xl:min-h-[50%] 2xl:mt-64 h-fit text-slate-100 backdrop-blur-md ml-auto lg:mt-24 xl:mt-28 text-sm md:text-base md:mt-auto border border-slate-100 rounded-xl p-4 md:p-6'>
-          <p className='mb-4 text-center lg:text-left'>
+          <div className='mb-4 text-center lg:text-left'>
             TACTECH New Media Solutions is a burgeoning assembly of acclaimed
             artists, forward-thinking visionaries, inventive coders, skillful
             animators, resourceful designers, and adept engineers.
-          </p>
-          <p className='mb-4 text-center lg:text-left'>
+          </div>
+          <div className='mb-4 text-center lg:text-left'>
             We are dedicated to creating and delivering immersive, interactive,
             and captivating experiences through the utilization of
             groundbreaking and transformative technologies.
-          </p>
-          <p className='mb-4 text-center lg:text-left'>
+          </div>
+          <div className='mb-4 text-center lg:text-left'>
             Our expertise spans across TTL Campaigns (both Online & Offine), XR,
             MR, AR, VR, Interactive installations, Immersive launches & Shows,
             along with temporary and permanent installations adaptable to a
             multitude of industries.
-          </p>
-          <p className='mb-4 text-center lg:text-left'>
+          </div>
+          <div className='mb-4 text-center lg:text-left'>
             As a collective, we are driven by our passion to transcend the
             conventional limits of new media solutions, crafting unforgettable
             experiences that make a lasting imprint
-          </p>
+          </div>
         </div>
       </div>
     </>
